d09bc5e6f300df19a33a408a61e269461162b51a
Justin <jjasa.2000@gmail.com>
2022-03-20 12:40:16 +1100

    small change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || '8080';
 
 const host = '0.0.0.0';
 
